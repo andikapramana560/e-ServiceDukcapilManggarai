f3ea9e8f75a5beafda53d05ba6bf2d6d73dcee8a
dika <andikapramana560@gmail.com>
2024-02-23 00:13:26 +0800

    fix kk model

--- app/Models/Kk.php
+++ app/Models/Kk.php
@@ -13,6 +13,3 @@ class Kk extends Model
         'id_penduduk', 'nama_kk', 'nik_kk', 'jns_kel_kk', 'tmp_lahir_kk', 'tgl_lahir_kk', 'agama_kk', 'pendidikan_kk', 'pekerjaan_kk', 'gol_darah_kk', 'status_pernikahan_kk', 'tgl_pernikahan_kk', 'stts_hub_klrg_kk', 'kewarganegaraan_kk', 'dok_imigrasi_kk', 'nama_ayah_kk', 'nama_ibu_kk', 'dok_ijin_tinggal', 'dok_akta_nikah', 'dok_fc_ktp', 'dok_fc_data_pendukung', 'dok_surat_pernyataan','tgl_pengajuan', 'keterangan', 'catatan', 'status'
     ];
 }
-
-git config --global user.email "you@example.com"
-  git config --global user.name "Your Name"
