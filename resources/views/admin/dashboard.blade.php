1888900e8858accbf1e968da648d22814a525c93
dika <andikapramana560@gmail.com>
2024-03-12 23:01:42 +0800

    layouting dashboard users

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,93 +1,102 @@
 @extends('layout.admin')
 
 @section('content')
-<main id="main" class="main">
+    <main id="main" class="main">
 
-    <div class="pagetitle">
-      <h1>Dashboard</h1>
-      <nav>
-        <ol class="breadcrumb">
-          <li class="breadcrumb-item"><a href="index.html">Home</a></li>
-          <li class="breadcrumb-item active">Dashboard</li>
-        </ol>
-      </nav>
-    </div><!-- End Page Title -->
+        <div class="pagetitle">
+            <h1>Dashboard</h1>
+            <nav>
+                <ol class="breadcrumb">
+                    <li class="breadcrumb-item"><a href="index.html">Home</a></li>
+                    <li class="breadcrumb-item active">Dashboard</li>
+                </ol>
+            </nav>
+        </div><!-- End Page Title -->
 
-    <section class="section dashboard">
-      <div class="row">
+        <section class="section dashboard">
+            <div class="row">
+                <!-- Left side columns -->
+                <div class="col-lg-12">
+                    <div class="row">
 
-        <!-- Left side columns -->
-        <div class="col-lg-12">
-          <div class="row">
+                        <!-- Sales Card -->
+                        <div class="col-md-6">
+                            <div class="card info-card sales-card">
+                                <div class="card-body">
+                                    <h3 class="card-title">Selamat Datang</h3>
+                                    <p>Sistem Informasi E-service Dinas Kependudukan dan Pencatatan Sipil Kabupaten
+                                        Manggarai.</p>
+                                    {{-- 
+                                  <div class="d-flex align-items-center">
+                                      <div
+                                          class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                          <i class="bi bi-cart"></i>
+                                      </div>
+                                      <div class="ps-3">
+                                          <h6>145</h6>
+                                          <span class="text-success small pt-1 fw-bold">12%</span> <span
+                                              class="text-muted small pt-2 ps-1">increase</span>
 
-            <!-- Sales Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card sales-card">
-                <div class="card-body">
-                  <h5 class="card-title">Sales <span>| Today</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-cart"></i>
+                                      </div>
+                                  </div> --}}
+                                </div>
+                            </div>
+                        </div><!-- End Sales Card -->
                     </div>
-                    <div class="ps-3">
-                      <h6>145</h6>
-                      <span class="text-success small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">increase</span>
+                </div><!-- End Left side columns -->
+                <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-xxl-4 col-md-6">
+                            <div class="card info-card sales-card">
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Jumlah Penduduk</h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-person"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145 Orang</h6>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
+                        <div class="col-xxl-4 col-md-6">
+                            <div class="card info-card sales-card">
+
+                                <div class="card-body">
+                                    <h5 class="card-title">Jumlah Pengajuan</h5>
+
+                                    <div class="d-flex align-items-center">
+                                        <div
+                                            class="card-icon rounded-circle d-flex align-items-center justify-content-center">
+                                            <i class="bi bi-file-earmark"></i>
+                                        </div>
+                                        <div class="ps-3">
+                                            <h6>145 Orang</h6>
+                                            <span class="text-primary small pt-1 fw-bold">10</span> <span
+                                                class="text-muted small pt-2 ps-1">Pengajuan KTP</span><br>
+                                            <span class="text-secondary small pt-1 fw-bold">10</span> <span
+                                                class="text-muted small pt-2 ps-1">Pengajuan KK</span><br>
+                                            <span class="text-success small pt-1 fw-bold">10</span> <span
+                                                class="text-muted small pt-2 ps-1">Pengajuan Akta Kelahiran</span><br>
+                                            <span class="text-warning small pt-1 fw-bold">10</span> <span
+                                                class="text-muted small pt-2 ps-1">Pengajuan Akta Kematian</span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div><!-- End Sales Card -->
 
                     </div>
-                  </div>
                 </div>
+            </div>
+        </section>
 
-              </div>
-            </div><!-- End Sales Card -->
-
-            <!-- Revenue Card -->
-            <div class="col-xxl-4 col-md-6">
-              <div class="card info-card revenue-card">
-                <div class="card-body">
-                  <h5 class="card-title">Revenue <span>| This Month</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-currency-dollar"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>$3,264</h6>
-                      <span class="text-success small pt-1 fw-bold">8%</span> <span class="text-muted small pt-2 ps-1">increase</span>
-
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-            </div><!-- End Revenue Card -->
-
-            <!-- Customers Card -->
-            <div class="col-xxl-4 col-xl-12">
-
-              <div class="card info-card customers-card">
-                <div class="card-body">
-                  <h5 class="card-title">Customers <span>| This Year</span></h5>
-
-                  <div class="d-flex align-items-center">
-                    <div class="card-icon rounded-circle d-flex align-items-center justify-content-center">
-                      <i class="bi bi-people"></i>
-                    </div>
-                    <div class="ps-3">
-                      <h6>1244</h6>
-                      <span class="text-danger small pt-1 fw-bold">12%</span> <span class="text-muted small pt-2 ps-1">decrease</span>
-
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div><!-- End Customers Card -->  
-          </div>
-        </div><!-- End Left side columns -->
-      </div>
-    </section>
-
-  </main><!-- End #main -->
+    </main><!-- End #main -->
 @endsection
-   
-    
